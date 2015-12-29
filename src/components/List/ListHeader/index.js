7ffabb202b0a1b5c8f74bc5038180c7853f158e3
Adam Mockor <adam.mockor@gmail.com>
2015-12-30 00:56:09 +0100

    List header with action

--- src/components/List/ListHeader/index.js
+++ src/components/List/ListHeader/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import CircleButton from './../../Button/CircleButton/';
+
 import styles from './styles.scss';
 
-export default function ListItem ({children}) {
+export default function ListHeader ({children, action, actionOnClick}) {
   return (
-    <li className={styles.root}>
+    <li className={styles.rootWithAction}>
       <h3>{children}</h3>
+      {action
+        ? <div className={styles.action} onClick={actionOnClick}>
+            <CircleButton>+</CircleButton>
+          </div>
+        : false}
     </li>
   );
 };
