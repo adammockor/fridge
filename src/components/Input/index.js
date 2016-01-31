dd91bdccbffe294680227053ed4050afb0b29612
Adam Mockor <adam.mockor@gmail.com>
2016-01-31 12:45:30 +0100

    input update

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,54 +6,55 @@ import styles from './styles.scss';
 let cx = classNames.bind(styles);
 
 export default class Input extends React.Component {
-  
+
   static propTypes = {
     type: React.PropTypes.string.isRequired,
     label: React.PropTypes.string.isRequired,
-    value: React.PropTypes.string
+    value: React.PropTypes.string,
+    onChange: React.PropTypes.func
   };
-  
-  constructor(props) {
+
+  constructor (props) {
     console.log(props);
     super(props);
-    
+
     this.state = {
       value: this.props.value || '',
       isFocused: false
     };
   }
-  
-  componentWillReceiveProps(nextProps) {
+
+  componentWillReceiveProps (nextProps) {
     this.setState({value: nextProps.value}); // update state when props change
   }
-  
-  handleChange(e) {
+
+  handleChange (e) {
     this.setState({value: e.target.value});
-    this.props.onChange(e.target.value); 
+    this.props.onChange(e.target.value);
   }
-  
-  onFocus() {
+
+  onFocus () {
     this.setState({isFocused: true});
   }
-  
-  onBlur() {
+
+  onBlur () {
     this.setState({isFocused: false});
   }
-  
-  render() {
+
+  render () {
     let label = cx({
-      labelActive: this.state.isFocused || this.state.value ,
+      labelActive: this.state.isFocused || this.state.value,
       label: !this.labelActive
     });
-    
+
     return (
       <div className={styles.root}>
-        <input 
-          className={styles.input} 
-          type={this.props.type} 
-          value={this.state.value} 
-          onChange={(e) =>this.handleChange(e)}
-          onFocus={() => this.onFocus()} 
+        <input
+          className={styles.input}
+          type={this.props.type}
+          value={this.state.value}
+          onChange={(e) => this.handleChange(e)}
+          onFocus={() => this.onFocus()}
           onBlur={() => this.onBlur()} />
         <label className={label}>{this.props.label}</label>
       </div>
