ea9830e7837a817a65f82ac4b4f736f41732dbb4
Adam Mockor <adam.mockor@gmail.com>
2016-01-31 12:45:48 +0100

    ListItem can contain actions

--- src/components/List/ListItem/index.js
+++ src/components/List/ListItem/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import styles from './styles.scss';
 
-export default function ListItem ({children}) {
+export default function ListItem ({children, actions}) {
   return (
-    <li className={styles.root}>{children}</li>
+    <li className={styles.root}>
+      <div className={styles.content}>{children}</div>
+      <div className={styles.actions}>{actions}</div>
+    </li>
   );
 };
